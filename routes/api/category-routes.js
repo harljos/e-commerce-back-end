bf85a96c2d0c715711ce2bceedf1f5e508d2e8f3
unknown <joseph.harley02@gmail.com>
2023-02-07 18:11:57 -0500

    put request at /api/categories/id updates category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,6 +25,7 @@ router.get('/:id', async (req, res) => {
 
     if (!category) {
       res.status(404).json({ message: "No category found with that id" });
+      return;
     }
     res.status(200).json(category);
   }
@@ -34,11 +35,27 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // creates a new category
+
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  // updates a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No category with that id" })
+      return;
+    }
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
