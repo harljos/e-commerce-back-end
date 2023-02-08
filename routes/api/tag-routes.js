01f730adf6cc1ac0f445dea0f9833928a966e953
unknown <joseph.harley02@gmail.com>
2023-02-07 20:56:16 -0500

    post request at api/tags creates new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,8 +33,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  // creates a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
