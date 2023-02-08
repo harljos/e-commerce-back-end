cf5490b60ddaec91eed1432cf4451a48c6a26b0a
unknown <joseph.harley02@gmail.com>
2023-02-07 20:50:18 -0500

    get request /api/tags/id finds tag based off given id with associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // finds a single tag by its `id` with its associated product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!tag) {
+      res.status(404).json({ message: "No tag found with that id" });
+      return;
+    }
+    res.status(200).json(tag);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
